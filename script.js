cd2331c8c69bbaa7d604d5ed9cea6dec52775c20
NILIMAINGLE <131347471+NILIMAINGLE@users.noreply.github.com>
2023-07-24 16:23:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function decimalToBinary(num) {
   //Write you code here
-  
-}
+	return decimal.toString(2)
+  }
 
-window.decimalToBinary = decimalToBinary;
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10));
+console.log(decimalToBinary(33));
